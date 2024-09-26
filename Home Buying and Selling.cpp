d081018e35d95c306cd0e5025e04274a6876aaae
AkramDevv <huseynliakramdevv1@gmail.com>
2024-09-26 11:04:39 +0400

    Update Home Buying and Selling.cpp

--- Home Buying and Selling.cpp
+++ Home Buying and Selling.cpp
@@ -294,6 +294,15 @@ int main() {
 
 	std::cout << "\nUser's budget: ";
 	std::cout << userBudget;
+	
+	
+	for (int i = 0;i < designRow;i++) {
+		for (int j = 0;j < designCol;j++) {
+			delete[] apartmentDesigns[i][j];
+		}
+		delete[] apartmentDesigns[i];
+	}
+	delete[] apartmentDesigns;
 
 
 
